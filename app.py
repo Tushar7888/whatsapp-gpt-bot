c2c5e477282cf947ec72c737679b87bcbc8b32ab
Tushar7888 <alkafreshbot@gmail.com>
2025-07-05 18:20:40 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import openai
+import os
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    incoming_msg = request.values.get('Body', '')
+    response = MessagingResponse()
+
+    reply = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": incoming_msg}]
+    )
+
+    msg = reply.choices[0].message['content']
+    response.message(msg)
+    return str(response)
+
+if __name__ == "__main__":
+    app.run()
